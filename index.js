8f3ea1c152ea51ddca2fe2df79459b2320a9dc5c
Matilde Rosa <77325729+matzro@users.noreply.github.com>
2023-08-05 21:11:38 +0100

    Added event listener

--- index.js
+++ index.js
@@ -1,14 +1,49 @@
+var crash = new Audio("./sounds/crash.mp3");
+var kick = new Audio("./sounds/kick-bass.mp3");
+var snare = new Audio("./sounds/snare.mp3");
+var tom1 = new Audio("./sounds/tom-1.mp3");
+var tom2 = new Audio("./sounds/tom-2.mp3");
+var tom3 = new Audio("./sounds/tom-3.mp3");
+var tom4 = new Audio("./sounds/tom-4.mp3");
+
+function detectKey() {
+    document.addEventListener("keydown", function (event) {
+        keyPressed = event.key;
+        switch (keyPressed) {
+            case "w":
+                crash.play();
+                break;
+            case "a":
+                tom4.play();
+                break;
+            case "s":
+                tom1.play();
+                break;
+            case "d":
+                snare.play();
+                break;
+            case "j":
+                kick.play();
+                break;
+            case "k":
+                tom2.play();
+                break;
+            case "l":
+                tom3.play();
+                break;
+        }
+    });
+}
+
+detectKey();
+
 function drum(instrument) {
-    var crash = new Audio("./sounds/crash.mp3");
-    var kick = new Audio("./sounds/kick-bass.mp3");
-    var snare = new Audio("./sounds/snare.mp3");
-    var tom1 = new Audio("./sounds/tom-1.mp3");
-    var tom2 = new Audio("./sounds/tom-2.mp3");
-    var tom3 = new Audio("./sounds/tom-3.mp3");
-    var tom4 = new Audio("./sounds/tom-4.mp3");
+
+
+    console.log(detectKey());
 
     switch (instrument) {
-        case "crash" :
+        case "crash":
             crash.play();
             break;
         case "kick":
@@ -31,3 +66,5 @@ function drum(instrument) {
             break;
     }
 }
+
+
